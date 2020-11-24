4a2b41c6d0bcdeb4b31def96f1155c356c5c7931
ConradoZA <zolen@gmail.com>
2020-11-24 15:38:15 +0100

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TableComponent } from './components/table/table.component';
+import { TableWrapperComponent } from './components/table-wrapper/table-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TableComponent
+    TableComponent,
+    TableWrapperComponent
   ],
   imports: [
     BrowserModule,
